d57a7c458d3da1151ff0d17d5bc5b2b3876266ae
Андрій <rukom@ukr.net>
2023-12-25 15:57:03 +0200

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -3,10 +3,30 @@ import css from './Modal.module.css';
 
 export class Modal extends Component {
 
+  handleBackdropClick = (event) => {
+    if (event.target === event.currentTarget) {
+      this.props.handleCloseModal();
+    }
+  }
+
+  handleKeyPress = (event) => {
+    if (event.code === 'Escape') {
+      this.props.handleCloseModal();
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyPress);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyPress)
+  }
+
   render() {
     const { name, age, isFavourite, } = this.props.modalData;
     return (
-      <div className={css.backdrop}>
+      <div className={css.backdrop} onClick={this.handleBackdropClick}>
         <div className={css.modal}>
           <h2 className={css['modal-title']}>Profile Details</h2>
           <p>Profile name: {name }</p>
